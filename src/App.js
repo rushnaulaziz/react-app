651b3a6269d742d38f4ec2d58f6f9906cb7ad48e
rushna nizami <rushna.nizami@senzai.ai>
2024-04-02 00:15:20 +0500

    basic react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="body1">Hello, World!</Typography>
+    </div>
+  );
+}
+
+export default App;
